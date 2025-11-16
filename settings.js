f084439dcab9897e4eed7df91eb98235f1a9ef52
sabtainwrites23-sudo <sabtainwrites23@gmail.com>
2025-11-16 17:13:57 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? 'Your SESSION ID Put' : process.env.SESSION_ID,
+SESSION_ID: process.env.ASITHA-MD=7f1b55f98158e5f1 === undefined ? 'Your SESSION ID Put' : process.env.SESSION_ID,
 PORT: process.env.PORT === undefined ? "8000" : process.env.PORT,
 SESSION_NAME: process.env.PORT === undefined ? "asitha" : process.env.SESSION_NAME,
 };
